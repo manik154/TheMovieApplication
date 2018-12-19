3e98c9c4d4a526bb3642efac17c2863b3c01b791
manik rastogi <manik.rastogi@compunnel.in>
2018-12-19 18:13:15 +0530

    eight commit

--- app/src/main/java/com/example/a13877/themovieapplication/api/ApiInterface.java
+++ app/src/main/java/com/example/a13877/themovieapplication/api/ApiInterface.java
@@ -9,6 +9,7 @@ import com.example.a13877.themovieapplication.Model.TvSeason;
 import com.example.a13877.themovieapplication.Model.TvShow;
 import com.example.a13877.themovieapplication.Model.VideoTrailers;
 import com.example.a13877.themovieapplication.util.Constant;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -38,47 +39,46 @@ public interface ApiInterface {
     Call<Movie> onAirTvShows(@Query("page") int page);
 
     //https://api.themoviedb.org/3/tv/60735?api_key=95ffbd804001d42bb7eb88c69bf5a9cd&language=en-US
-    @GET(Constant.TV_PATH +"/{tv_id}")
-    Call<TvShow> tvDetail(@Path("tv_id")int tvId);
+    @GET(Constant.TV_PATH + "/{tv_id}")
+    Call<TvShow> tvDetail(@Path("tv_id") int tvId);
 
-    @GET(Constant.TV_PATH +"/{tv_id}")
-    Call<TvSeason> tvDetail2(@Path("tv_id")int tvId);
+    @GET(Constant.TV_PATH + "/{tv_id}")
+    Call<TvSeason> tvDetail2(@Path("tv_id") int tvId);
 
-    @GET(Constant.TV_PATH +"/{tv_id}"+"/season"+"/{season_number}")
-    Call<Season>seasonDetail(@Path("tv_id")int tvId, @Path("season_number")int season_number);
+    @GET(Constant.TV_PATH + "/{tv_id}" + "/season" + "/{season_number}")
+    Call<Season> seasonDetail(@Path("tv_id") int tvId, @Path("season_number") int season_number);
 
-    @GET(Constant.MOVIE_PATH+"/{movie_id}")
-    Call<MovieDetails>movieDetail(@Path("movie_id") int movieId);
+    @GET(Constant.MOVIE_PATH + "/{movie_id}")
+    Call<MovieDetails> movieDetail(@Path("movie_id") int movieId);
 
     //https://api.themoviedb.org/3/movie/335983/reviews?api_key=95ffbd804001d42bb7eb88c69bf5a9cd&language=en-US&page=1
-@GET(Constant.MOVIE_PATH+ "/{movie_id}"+Constant.REVIEWS)
-    Call<Review>reviewsAbout(@Path("movie_id")int movieId);
+    @GET(Constant.MOVIE_PATH + "/{movie_id}" + Constant.REVIEWS)
+    Call<Review> reviewsAbout(@Path("movie_id") int movieId);
 
     //https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=<<api_key>>&language=en-US
-    @GET(Constant.MOVIE_PATH+ "/{movie_id}"+"/similar")
-    Call<GetSimilar>similarMovies(@Path("movie_id")int movieId);
+    @GET(Constant.MOVIE_PATH + "/{movie_id}" + "/similar")
+    Call<GetSimilar> similarMovies(@Path("movie_id") int movieId);
 
     //https://api.themoviedb.org/3/tv/{tv_id}/similar?api_key=<<api_key>>&language=en-US&page=1
-    @GET(Constant.TV_PATH+ "/{tv_id}"+"/similar")
-    Call<GetSimilar>similarTvShows(@Path("tv_id")int tvId);
+    @GET(Constant.TV_PATH + "/{tv_id}" + "/similar")
+    Call<GetSimilar> similarTvShows(@Path("tv_id") int tvId);
 
     //https://api.themoviedb.org/3/tv/{tv_id}/reviews?api_key=<<api_key>>&language=en-US&page=1
-    @GET(Constant.TV_PATH+ "/{tv_id}"+Constant.REVIEWS)
-    Call<Review>getTvShowReview(@Path("tv_id")int tvId);
+    @GET(Constant.TV_PATH + "/{tv_id}" + Constant.REVIEWS)
+    Call<Review> getTvShowReview(@Path("tv_id") int tvId);
 
     //https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=<<api_key>>&language=en-US
 
-    @GET(Constant.MOVIE_PATH+ "/{movie_id}"+Constant.VIDEOS)
-    Call<VideoTrailers>getVideoTrailers(@Path("movie_id")int movieId);
+    @GET(Constant.MOVIE_PATH + "/{movie_id}" + Constant.VIDEOS)
+    Call<VideoTrailers> getVideoTrailers(@Path("movie_id") int movieId);
 
 
     //https://api.themoviedb.org/3/genre/movie/list?api_key=95ffbd804001d42bb7eb88c69bf5a9cd&language=en-US
-    @GET(Constant.GENRE_PATH+"/list")
-    Call<TvShow>getGenreList();
+    @GET(Constant.GENRE_PATH + "/list")
+    Call<TvShow> getGenreList();
 
-    @GET(Constant.GENRE_PATH2+"/list")
-    Call<TvShow>getTvGenreList();
-
+    @GET(Constant.GENRE_PATH2 + "/list")
+    Call<TvShow> getTvGenreList();
 
 
 }
