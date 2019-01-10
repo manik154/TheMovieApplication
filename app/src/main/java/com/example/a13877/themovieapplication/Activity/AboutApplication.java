4dfb0273fcf3fee7161c111c417b6f9427862701
manik rastogi <manik.rastogi@compunnel.in>
2019-01-10 10:18:21 +0530

    final commmit

--- app/src/main/java/com/example/a13877/themovieapplication/Activity/AboutApplication.java
+++ app/src/main/java/com/example/a13877/themovieapplication/Activity/AboutApplication.java
@@ -1,11 +1,9 @@
 package com.example.a13877.themovieapplication.Activity;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import com.example.a13877.themovieapplication.R;
@@ -22,7 +20,7 @@ public class AboutApplication extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("About Application");
         webView=findViewById(R.id.webview);
-       // Typeface font = Typeface.createFromAsset(, "");
+
         String data = "<html><body><p1><i>The MovieApplication is is one of the best informative application for upcoming TV episodes and movies . This application delivers the best experience anywhere, anytime to its user<i></p1></body></html>";
         webView.loadData(data, "text/html", "UTF-8");
 
